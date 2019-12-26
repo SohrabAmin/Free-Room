2a161440c96a3e23eb6faca323caee9b613294b6
SohrabAmin <sohrobo@hotmail.com>
2019-12-25 21:38:33 -0500

    Add Union of Time Intervals for Room Schedule

--- Free-Room.py
+++ Free-Room.py
@@ -1,6 +1,6 @@
-from datetime import datetime
 import os
 import json
+from datetime import datetime
 
 
 class Course:
@@ -31,8 +31,10 @@ class Event:
         self.section = json['code']
         self.location = json['times'][0]['location']
         self.day = json['times'][0]['day']
-        self.start_time = datetime.strptime(str(int(json['times'][0]['start'])//3600), '%H').strftime('%I:%M %p')
-        self.end_time = datetime.strptime(str(int(json['times'][0]['end'])//3600), '%H').strftime('%I:%M %p')
+        self.start_time = json['times'][0]['start']//3600
+        self.end_time = json['times'][0]['end']//3600
+        #self.start_time = datetime.strptime(str(int(json['times'][0]['start'])//3600), '%H').strftime('%I:%M %p')
+        #self.end_time = datetime.strptime(str(int(json['times'][0]['end'])//3600), '%H').strftime('%I:%M %p')
 
 
 class Building:
@@ -51,6 +53,35 @@ class Room:
     def __init__(self, number):
         self.number = number
         self.schedule = []
+
+
+def merge_intervals(arr):
+
+    # Sorting based on the increasing order
+    # of the start intervals
+    arr.sort(key=lambda x: x[0])
+
+    # array to hold the merged intervals
+    m = []
+    s = -10000
+    max = -100000
+    for i in range(len(arr)):
+        a = arr[i]
+        if a[0] > max:
+            if i != 0:
+                m.append([s, max])
+            max = a[1]
+            s = a[0]
+        else:
+            if a[1] >= max:
+                max = a[1]
+
+    if max != -100000 and [s, max] not in m:
+        m.append([s, max])
+    # print("The Merged Intervals are :", end=" ")
+    # for i in range(len(m)):
+    #     print(m[i], end=" ")
+    return m
 
 
 if __name__ == '__main__':
@@ -76,9 +107,12 @@ if __name__ == '__main__':
                 if event.location not in Rooms:
                     Rooms[event.location] = Room(event.location)
                 else:
-                    Rooms[event.location].schedule.append(event.code +' '+event.start_time+'-'+event.end_time)
+                    Rooms[event.location].schedule.append([event.start_time, event.end_time])
 
+    # Merge Intervals in Schedule
+    for room in Rooms:
+        Rooms[room].schedule = merge_intervals(Rooms[room].schedule)
+
+    # Print all Rooms and Times
     for room in Rooms:
         print(room, Rooms[room].schedule)
-
-
